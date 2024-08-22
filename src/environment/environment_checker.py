3fcde82e80db08e47e329ec755af0f4942fe8a25
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-08-22 19:40:18 +0100

    first commit

---
+++ src/environment/environment_checker.py
@@ -0,0 +1,145 @@
+"""
+This module contains functions to fetch weather data from two sources: 
+    the OpenWeatherMap API and the BME280 sensor.
+
+Functions:
+    __open_weather_api(latitude,longitude,key): 
+        Fetches weather data from the OpenWeatherMap API.
+    __bme_280(port, address): 
+        Fetches weather data from the BME280 sensor.
+
+The __open_weather_api function takes in latitude, longitude, and an API key, 
+and returns a tuple containing the temperature, pressure, and humidity at 
+the specified location.
+
+The __bme_280 function takes in a port and an address, and returns a tuple 
+containing the temperature, pressure, and humidity from the BME280 sensor 
+connected at the specified port and address.
+"""
+
+import argparse
+import csv
+import io
+import os
+from datetime import datetime
+
+import bme280
+import requests
+import smbus2
+from meteocalc import dew_point
+
+
+def __open_weather_api(latitude, longitude, key):
+    """
+    Fetches weather data from the OpenWeatherMap API.
+
+    Args:
+        latitude (float): The latitude of the location.
+        longitude (float): The longitude of the location.
+        key (str): The API key for OpenWeatherMap.
+
+    Returns:
+        tuple: The temperature, pressure, and humidity at the location.
+    """
+    call = "https://api.openweathermap.org/data/2.5/weather?"
+    location_part = f"lat={latitude}&lon={longitude}"
+    api = f"{call}{location_part}&appid={key}&units=metric"
+    response = requests.get(api, timeout=10)
+    json_data = response.json()
+    return (
+        json_data["main"]["temp"],
+        json_data["main"]["pressure"],
+        json_data["main"]["humidity"],
+    )
+
+
+def __bme_280(address):
+    """
+    Fetches weather data from the BME280 sensor.
+
+    Args:
+        address (str): The address of the sensor.
+
+    Returns:
+        tuple: The temperature, pressure, and humidity from the sensor.
+    """
+    bus = smbus2.SMBus(1)
+    calibration_params = bme280.load_calibration_params(bus, address)
+    data = bme280.sample(bus, address, calibration_params)
+
+    return round(data.temperature, 2), round(data.pressure, 2), round(data.humidity, 2)
+
+
+def get_data(api, lat, lon, file_location, address):
+    """
+    Fetches weather data and writes it to a file.
+
+    Args:
+        api (str): The API key for OpenWeatherMap. 
+                If this is None, data is fetched from the BME280 sensor.
+        lat (float): The latitude of the location.
+        lon (float): The longitude of the location.
+        file_location (str): The location of the file to which the data is written.
+        address (str): The address of the BME280 sensor.
+    """
+    if api:
+        temperature, pressure, humidity = __open_weather_api(lat, lon, api)
+    else:
+        temperature, pressure, humidity = __bme_280(address)
+
+    formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M")
+    get_dew_point = round(dew_point(temperature, humidity), 2)
+    with io.open(file_location, "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        if os.stat(file_location).st_size == 0:
+            writer.writerow(
+                ["date-time", "temperature", "pressure", "humidity", "dew-point"]
+            )
+        writer.writerow(
+            [formatted_date, temperature, pressure, humidity, get_dew_point]
+        )
+
+
+def __parse_args():
+    """
+    Parses command line arguments.
+
+    This function uses argparse to parse command line arguments and returns an argparse.
+
+    Returns:
+        argparse.Namespace: An object that holds the command line arguments as attributes. 
+            This object will have the following attributes:
+            - api (str): The API key for OpenWeatherMap.
+            - lat (float): The latitude for weather data.
+            - lon (float): The longitude for weather data.
+            - file (str): The file location for output.
+            - port (int): The port for the BME280 sensor.
+            - address (str): The address for the BME280 sensor.
+    """
+    parser = argparse.ArgumentParser(description="Environment Checker")
+    parser.add_argument(
+        "--api", type=str, required=False, help="API key for OpenWeatherMap"
+    )
+    parser.add_argument(
+        "--lat", type=float, required=False, help="Latitude for weather data"
+    )
+    parser.add_argument(
+        "--lon", type=float, required=False, help="Longitude for weather data"
+    )
+    parser.add_argument(
+        "--file", type=str, required=False, help="File location for output"
+    )
+    parser.add_argument(
+        "--address",
+        type=str,
+        required=False,
+        default="0x77",
+        help="Address for BME280 sensor",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = __parse_args()
+    get_data(args.api, args.lat, args.lon, args.file, args.address)
---
+++ tests/test_environment-checker.py
@@ -0,0 +1,101 @@
+import argparse
+from unittest.mock import patch, MagicMock, mock_open
+from src.environment.environment_checker import (
+    __parse_args,
+    __open_weather_api,
+    __bme_280,
+    get_data,
+)
+
+
+@patch("requests.get")
+def test_open_weather_api(mock_get):
+    # Mock the response from the API
+    mock_response = {"main": {"temp": 25.5, "pressure": 1013, "humidity": 70}}
+    mock_get.return_value.json.return_value = mock_response
+
+    # Call the function with sample latitude, longitude, and API key
+    temperature, pressure, humidity = __open_weather_api(
+        37.7749, -122.4194, "your-api-key"
+    )
+
+    # Assert that the function returns the expected values
+    assert temperature == 25.5
+    assert pressure == 1013
+    assert humidity == 70
+
+
+@patch("smbus2.SMBus")
+@patch("bme280.load_calibration_params")
+@patch("bme280.sample")
+def test_bme_280(mock_sample, mock_load_calibration_params, mock_smbus):
+    # Arrange
+    mock_load_calibration_params.return_value = MagicMock()
+    mock_sample.return_value.temperature = 20.0
+    mock_sample.return_value.pressure = 1000.0
+    mock_sample.return_value.humidity = 50.0
+    address = 0x77
+
+    # Act
+    temperature, pressure, humidity = __bme_280(address)
+
+    # Assert
+    mock_smbus.assert_called_once_with(1)
+    mock_load_calibration_params.assert_called_once_with(
+        mock_smbus.return_value, address
+    )
+    mock_sample.assert_called_once_with(
+        mock_smbus.return_value, address, mock_load_calibration_params.return_value
+    )
+
+    assert temperature == 20.0
+    assert pressure == 1000.0
+    assert humidity == 50.0
+
+
+@patch("requests.get")
+@patch("src.environment.environment_checker.os.stat")
+def test_get_data_api(mock_stat, mock_api):
+    mock_response = {"main": {"temp": 25.5, "pressure": 1013, "humidity": 70}}
+    mock_api.return_value.json.return_value = mock_response
+    mock_stat.return_value.st_size = 0
+    mock_file = mock_open()
+    with patch("src.environment.environment_checker.io.open", mock_file, create=True):
+        get_data("api_key", "lat", "lon", "file_location","address")
+    mock_file.assert_called_once_with(
+        "file_location", "a", newline="", encoding="utf-8"
+    )
+
+
+@patch("smbus2.SMBus")
+@patch("bme280.load_calibration_params")
+@patch("bme280.sample")
+@patch("src.environment.environment_checker.os.stat")
+def test_get_data_bme(mock_stat, mock_sample, mock_load_calibration_params, mock_smbus):
+    # Arrange
+    mock_load_calibration_params.return_value = MagicMock()
+    mock_sample.return_value.temperature = 20.0
+    mock_sample.return_value.pressure = 1000.0
+    mock_sample.return_value.humidity = 50.0
+    mock_file = mock_open()
+    mock_stat.return_value.st_size = 0
+
+    with patch("src.environment.environment_checker.io.open", mock_file, create=True):
+        get_data(None, "lat", "lon", "file_location", "address")
+        mock_file.assert_called_once_with(
+            "file_location", "a", newline="", encoding="utf-8"
+        )
+
+
+@patch("argparse.ArgumentParser.parse_args")
+def test_parse_args(mock_parse_args):
+    mock_parse_args.return_value = argparse.Namespace(
+        api="api_key", lat=1.0, lon=1.0, file="file_location", port=1, address="0x77"
+    )
+    result = __parse_args()
+    assert result.api == "api_key"
+    assert result.lat == 1.0
+    assert result.lon == 1.0
+    assert result.file == "file_location"
+    assert result.port == 1
+    assert result.address == "0x77"
